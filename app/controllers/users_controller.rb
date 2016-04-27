54aac2689d7e4cfb76a1c85e080a724745b8ae81
artem_galas <artem.galas@gmail.com>
2016-04-27 17:39:38 +0300

    add shown all tickets on profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     # List Tickets
     # curl https://playpenlabs.zendesk.com/api/v2/tickets.json \
     # -v -u zendesk@playpenlabs.com:...
-    @tiket = HTTP.basic_auth(user: current_user.zendesk_email, pass: current_user.zendesk_password).get('https://playpenlabs.zendesk.com/api/v2/tickets.json')
+    @tickets = JSON.parse(HTTP.basic_auth(user: current_user.zendesk_email, pass: current_user.zendesk_password).get('https://playpenlabs.zendesk.com/api/v2/tickets.json'))['tickets']
   end
 
 end
