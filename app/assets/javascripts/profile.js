08b45b3587fbf66124e663be942b854fdad22889
Mehreen Tahir <mehreen.tahir@square63.com>
2016-08-05 16:39:18 +0500

    User: Copy changes on profile page for user

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -18,12 +18,11 @@ function get_ticket_by_id() {
   $.get('/users/get_ticket_by_id/' + targets_array[index]).then(function (response) {
     if ($('div.row.ticket .user-ticket').length > 0) {
       $('div.row.ticket .user-ticket').replaceWith(response);
-      $('.col-ticket .counter-email').replaceWith('<span>'+ (index+1) + '/</span><span>' + targets_array.length + '</span>')
     }
     else {
       $('div.row.ticket').append(response);
-      $('.col-ticket .counter-email').append('<span>'+ (index+1) + '/</span><span>' + targets_array.length + '</span>')
     }
+    $('.col-ticket .counter-email').replaceWith(['<span>', 'Email ', (index+1), '/</span><span>', targets_array.length, '</span>'].join(''))
   })
 }
 
