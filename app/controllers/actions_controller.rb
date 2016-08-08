8915d9c736a9f64e09085c8b286b80539261fa04
Mehreen Tahir <mehreen.tahir@square63.com>
2016-08-08 15:08:24 +0500

    Action: Added flash messages + code refactored & meaningfull names used

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,30 +1,43 @@
 class ActionsController < ApplicationController
+  before_action :set_action, only: [:edit, :update, :destroy]
 
   def index
     @a = current_user.action_messages
   end
 
   def new
-    @a = ActionMessage.new()
+    @action = ActionMessage.new()
   end
 
   def create
-    a = current_user.action_messages.create(action_message_params_permit)
-    redirect_to action_messages_path
+    @action = current_user.action_messages.new(action_message_params_permit)
+    if @action.save
+      redirect_to action_messages_path, notice: 'Action was successfully created.'
+    else
+      flash[:error] = @action.errors.full_messages.join
+      render :new
+    end
   end
 
   def edit
-    @a = ActionMessage.find(params[:id])
   end
 
   def update
-    a = ActionMessage.find(params[:id])
-    a.update(action_message_params_permit)
-    redirect_to action_messages_path
+    if @action.update(action_message_params_permit)
+      redirect_to action_messages_path, notice: 'Action was successfully updated.'
+    else
+      flash[:error] = @action.errors.full_messages.join
+      render :edit
+    end
   end
 
   def destroy
-    ActionMessage.find(params[:id]).destroy
+    if @action.reports.present?
+      flash[:error] = 'It has reports so it can not be deleted'
+    else
+      flash[:notice] = 'Action was successfully deleted.'
+      @action.destroy
+    end
     redirect_to action_messages_path
   end
 
@@ -37,6 +50,10 @@ class ActionsController < ApplicationController
 
   def action_message_params_permit
     params.require(:action_message).permit(:name, :recipient)
+  end
+
+  def set_action
+    @action = ActionMessage.find(params[:id])
   end
 
 end
