4722c6350100a2e329c15c9591f7214c98019d85
Mehreen Tahir <mehreen.tahir@square63.com>
2016-08-08 11:34:43 +0500

    User: Registeration controller for devise added + after sign up path configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   root 'home#index'
 
   resources :home do
