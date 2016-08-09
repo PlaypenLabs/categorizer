24d218a837a1aa55d2830d63119c730b5329ad8c
Mehreen Tahir <mehreen.tahir@square63.com>
2016-08-09 13:07:43 +0500

    ActionMessage: Added validation for valid recipent email

--- app/models/action_message.rb
+++ app/models/action_message.rb
@@ -4,4 +4,5 @@ class ActionMessage < ActiveRecord::Base
   has_many :tickets
 
   validates :name, presence: true
+  validates :recipient, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: ' email format is invalid' }, if: lambda { |recipient| self.recipient.present? }
 end
