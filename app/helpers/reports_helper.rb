a8c7df4c9f6a31f60ba7e2e3da6457f5689f1e5b
Mehreen Tahir <mehreen.tahir@square63.com>
2016-08-10 10:48:33 +0500

    Reports: Added check for current user presence for intital text on report

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,5 +1,5 @@
 module ReportsHelper
   def get_initial_report_text
-    'Here is an example of what your weekly report would look like' if current_user.first_login?
+    'Here is an example of what your weekly report would look like' if current_user.present? && current_user.first_login?
   end
 end
