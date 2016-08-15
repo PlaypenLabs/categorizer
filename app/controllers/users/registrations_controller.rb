a315a198bf8509097a83eb42712eef7e080c5d00
Mehreen Tahir <mehreen.tahir@square63.com>
2016-08-15 16:40:02 +0500

    User: Organization found/created and associated with user on time of signup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,13 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  protected
+  def create
+    super do
+      organization = Organization.find_or_create_by(name: URI.encode(params[:organization_name].strip)) if params[:organization_name].present?
+      resource.organization = organization
+      resource.save
+    end
+  end
 
+  protected
   def after_sign_up_path_for(resource)
     welcome_url
   end
