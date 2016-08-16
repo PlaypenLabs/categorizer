b143f713a7da9139291913776ce1c6e5fb239a55
Mehreen Tahir <mehreen.tahir@square63.com>
2016-08-16 15:06:47 +0500

    User: Authenticated user from zendesk API before creating user & organization

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def create
     super do
-      organization = Organization.find_or_create_by(name: URI.encode(params[:organization_name].strip)) if params[:organization_name].present?
+      unless valid_organization?
+        @user.errors.messages[:organization_id].clear
+        flash[:alert] = 'Invalid zendesk credentials.'
+        return render :new
+      end
+      organization = Organization.find_or_create_by(name: params[:organization_name]) if params[:organization_name].present?
       resource.organization = organization
       resource.save
     end
@@ -10,5 +15,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
   def after_sign_up_path_for(resource)
     welcome_url
+  end
+
+  def valid_organization?
+    begin
+      url = "https://#{params[:organization_name]}.zendesk.com/api/v2/organization_memberships.json"
+      HTTP.basic_auth(user: params[:user][:zendesk_email], pass: params[:user][:zendesk_password]).get(url).code == 200
+    rescue StandardError
+      return false
+    end
   end
 end
