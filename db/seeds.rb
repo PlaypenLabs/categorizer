028eba40b008cac5d47580f1d1ffbc2750d868c3
Mehreen Tahir <mehreen.tahir@square63.com>
2016-08-16 18:42:14 +0500

    Seed: Updated the seed file with organization name

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
-organization = Organization.find_or_create_by!(name: 'playpenlabs')
+organization = Organization.find_or_create_by!(name: 'playpenlabs.zendesk.com')
 User.create!(email: 'admin@categorizer.com', password: 'categorizer', password_confirmation: 'categorizer', zendesk_email: 'zendesk@playpenlabs.com', zendesk_password: 'devzd', organization: organization)
