103c6ae9d3211c2b977db47ff2f25a5fe202e4d8
Mehreen Tahir <mehreen.tahir@square63.com>
2016-11-15 11:16:34 +0500

    Ticket: Added date as column & filter in admin pannel of tickets

--- app/admin/ticket.rb
+++ app/admin/ticket.rb
@@ -13,6 +13,7 @@ ActiveAdmin.register Ticket do
       link_to user.email, admin_user_path(user) if user.present?
     end
     column :created_at
+    column :date
     actions
   end
 
@@ -20,5 +21,6 @@ ActiveAdmin.register Ticket do
   filter :description
   filter :from
   filter :created_at
+  filter :date
   filter :categorize_by_users_in, label: 'Having user', as: :select, collection: proc { User.all.collect{ |user| [user.email, user.id] } }
 end
